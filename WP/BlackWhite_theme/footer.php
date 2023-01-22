38130547fad883fe05b1ae2ba5bb4ffbe54b5f1a
Artem Goncharenko <goncharenko.art@gmail.com>
2023-01-22 00:33:07 -0500

    Wordpress theme to transfer HTML, CSS, JS website

---
+++ WP/BlackWhite_theme/footer.php
@@ -0,0 +1,24 @@
+<footer class="footer">
+    <section class="footerPhone">
+        <h3>Get in touch</h3>
+        <a href="tel:(212)814-6088" class="phone"> <img src="icons/phone.svg" alt="phone icon" class="icon">
+            Phone: (212)814-6088</a>
+        <a href="https://goo.gl/maps/2oF1hd5kvwmJXkR2A"
+           target="_blank"
+           rel="noopener noreferrer"
+           class="address"> <img src="icons/map.svg" alt="map icon"
+                                 class="icon">
+            533 3rd Avenue, New York, NY</a>
+    </section>
+    <section class="footerFollow">
+        <h3 class="follow">Follow us Online!</h3>
+        <a href="https://instagram.com/jm_hair_extention?igshid=Zjc2ZTc4Nzk="
+           target="_blank"
+           rel="noopener noreferrer"
+           class="insta"><img src="icons/insta.svg"
+                              alt="instagram logo" class="icon"> Instagram</a>
+    </section>
+</footer>
+<script src="js/toggleButton.js"></script>
+</body>
+</html>
---
+++ WP/BlackWhite_theme/header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:title" content="Invisible Hair extension"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="https://invisiblehairextension.com"/>
+    <meta property="og:image" content="https://invisiblehairextension.com/images/July.jpg"/>
+    <meta property="og:local" content="en_US"/>
+    <meta property="og:description"
+          content="Unleash your hair's full potential with our personalized approach to high-quality, 100% Slavic hair extensions. We specialize in a variety of extension methods to give you the perfect look tailored to your individual needs. Book your appointment today!"/>
+    <link rel="stylesheet" type="text/css" href="styles/navbar.css">
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <link rel="stylesheet" type="text/css" href="styles/footer.css">
+    <style> @import url('https://fonts.googleapis.com/css2?family=Gotu&display=swap'); </style>
+    <title>Invisible Hair Extensions</title>
+</head>
+<body>
+<nav class="navbar">
+    <div class="brand-title">Invisible Hair Extension</div>
+    <a href="#" class="toggle-button">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </a>
+    <div class="navbar-links">
+        <ul>
+            <li class="hover-underline"><a href="./index.html">Home</a></li>
+            <li class="hover-underline"><a href="./about.html">About</a></li>
+            <li class="hover-underline"><a href="./portfolio.html">Portfolio</a></li>
+            <li class="hover-underline"><a href="./prices.html">Prices</a></li>
+            <li class="hover-underline"><a href="./contact.html">Contact</a></li>
+        </ul>
+    </div>
+</nav>
